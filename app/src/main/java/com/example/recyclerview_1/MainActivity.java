6c6ac59d35ba82fcf8bd9d72a99aff341e730e83
student_ait409 <talyshevmihail06@gmail.com>
2023-10-25 16:41:41 +0300

    Initial commit

---
+++ app/src/main/java/com/example/recyclerview_1/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.recyclerview_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ArrayList<RecyclerViewItem> recyclerViewItems = new ArrayList<>();
+        recyclerViewItems.add(new RecyclerViewItem(R.drawable.baseline_cruelty_free_24,
+                "Rabbit", "Life is rabbit!"));
+        recyclerViewItems.add(new RecyclerViewItem(R.drawable.baseline_coronavirus_24,
+                "Coronavirus", "Life is coronavirus!"));
+        recyclerViewItems.add(new RecyclerViewItem(R.drawable.baseline_emoji_nature_24,
+                "Bee", "Life is bee!"));
+    }
+}
