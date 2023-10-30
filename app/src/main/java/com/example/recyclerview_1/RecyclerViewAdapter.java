5ac02ea171f34d1981b7edfd78398ad43cf5cb1a
student_ait409 <talyshevmihail06@gmail.com>
2023-10-30 10:01:28 +0300

    RecyclerView part 2

---
+++ app/src/main/java/com/example/recyclerview_1/RecyclerViewAdapter.java
@@ -0,0 +1,47 @@
+package com.example.recyclerview_1;
+
+import android.text.Layout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.RecyclerViewViewHolder> {
+
+    public static class RecyclerViewViewHolder extends RecyclerView.ViewHolder {
+
+        public ImageView imageView;
+        public TextView textView1;
+        public TextView textView2;
+
+
+        public RecyclerViewViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imageView = itemView.findViewById(R.id.imageView);
+            textView1 = itemView.findViewById(R.id.textView1);
+            textView2 = itemView.findViewById(R.id.textView2);
+        }
+    }
+
+    @NonNull
+    @Override
+    public RecyclerViewViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.recycler_view_item, parent, false);
+        RecyclerViewViewHolder recyclerViewViewHolder = new RecyclerViewViewHolder(view);
+        return recyclerViewViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerViewViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
